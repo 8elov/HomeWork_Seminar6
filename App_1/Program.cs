ebdbcecf6342b7d69b8d6d347536ea876ea79933
Nikita Belov <lolvvto@yandex.ru>
2022-11-17 16:34:10 +0700

    Add App 1

---
+++ App_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Какое количество чисел вы хотите ввести? ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[M];
+
+int GetPositive(int M)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите " + (i + 1) + " число:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int sumPositive = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sumPositive ++;
+        }
+    }
+    return sumPositive;
+}
+
+Console.WriteLine("Количество чисел больше 0 = " + GetPositive(M)); 
